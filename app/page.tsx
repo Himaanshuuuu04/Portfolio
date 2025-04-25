4861f83c8cf745f65ec5a5017224c23029ebfab8
Himaansh_uu <himan0411singh@gmail.com>
2025-04-25 21:21:32 +0530

    chore: update dependencies and devDependencies in package.json
    - Added new dependencies: @radix-ui/react-slot, @react-spring/web, @tabler/icons-react, @tsparticles/engine, @tsparticles/react, @tsparticles/slim, class-variance-authority, clsx, framer-motion, lucide-react, motion, next-themes, primeicons, and tailwind-merge.
    - Updated existing dependencies: simple-icons.
    - Updated devDependencies: added @eslint/eslintrc, updated @tailwindcss/postcss, and added tw-animate-css.
    - Ensured compatibility with React 19 and Next.js 15.3.1.

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,98 @@
 import Image from "next/image";
+import { House, SquareUserRound, SquareCode, Mail } from "lucide-react";
+import { AnimatePresence, motion } from "motion/react";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import SparklesPreview from "@/components/demo/sparklePreviewDemo";
+import Squares from "@/components/ui/Squares/Squares";
+import AnimatedButton from "@/components/ui/AnimatedButton";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className="flex flex-col bg-black items-center justify-items-center h-screen w-screen overflow-hidden p-8 pb-20 gap-10  font-[family-name:var(--font-geist-sans)]">
+      <div className="absolute inset-0 w-full h-full bg-black">
+        <Squares
+          speed={0.4}
+          squareSize={40}
+          direction="diagonal" // up, down, left, right, diagonal
+          borderColor="rgb(41,41,41)"
+          hoverFillColor="#444444"
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      </div>
+      <FloatingNav navItems={navItems} />
+      <main className="flex flex-col gap-[32px] row-start-2 items-center  ">
+        <div className="flex flex-col gap-2 items-center justify-center mt-20 md:mt-50">
+          <SparklesPreview>
+            <p className="text-base text-wrap w-screen px-5 md:w-full sm:text-lg font-light text-center text-neutral-500 dark:text-white">
+              Hey, I'm a
+              <strong className="text-green-400 font-bold">
+                {" "}
+                Full Stack Developer
+              </strong>{" "}
+              passionate about building performant, user-friendly, and scalable
+              applications.
+            </p>
+          </SparklesPreview>
+          <div className="flex flex-col md:flex-row gap-2 items-center justify-center -mt-4">
+           
+            <AnimatedButton>
+              <a
+                href="https://drive.google.com/file/d/1q2r3s4t5u6v7w8x9y0z1a2b3c4d5e6f/view?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Let's Connect ✋
+              </a>
+              
+            </AnimatedButton>
+             <AnimatedButton>
+              <a
+                href="https://drive.google.com/file/d/1q2r3s4t5u6v7w8x9y0z1a2b3c4d5e6f/view?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Resume 📄
+              </a>
+              
+            </AnimatedButton>
+            </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
     </div>
   );
 }
+const navItems = [
+  {
+    name: "Home",
+    link: "/",
+    icon: <House className="h-5 w-5 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "About",
+    link: "/about",
+    icon: (
+      <SquareUserRound className="h-5 w-5 text-neutral-500 dark:text-white" />
+    ),
+  },
+
+  {
+    name: "Projects",
+    link: "/projects",
+    icon: <SquareCode className="h-5 w-5 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "Contact",
+    link: "/contact",
+    icon: <Mail className="h-5 w-5 text-neutral-500 dark:text-white" />,
+  },
+  {
+    name: "LinkedIn",
+    link: "/linkedin",
+    icon: <i className="pi pi-linkedin  " style={{ fontSize: "1rem" }}></i>,
+  },
+  {
+    name: "GitHub",
+    link: "/github",
+    icon: <i className="pi pi-github" style={{ fontSize: "1rem" }}></i>,
+  },
+];
